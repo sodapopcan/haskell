72683abc98ccc80eba09a42563804fb3ccc46fdf
Andrew Haust <andrewwhhaust@gmail.com>
2018-02-09 13:47:44 -0500

    Add tuple/list comp examples right from LYAH

--- lyahfgg/01-starting-out.hs
+++ lyahfgg/01-starting-out.hs
@@ -126,3 +126,8 @@ aheadOfMyself a b =
 --    but zip being available in ruby seems to cater to the functional crowd as
 --    I've tried and never actually needed zip in ruby ever.
 -- 
+-- These examples are right from the book... I didn't change them
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]   
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+--
