0e0467393ebfc3eae6a32bfb268a85e4ea29a2f8
Andrew Haust <andrewwhhaust@gmail.com>
2018-03-01 19:59:36 -0500

    Another implementation of i/o loop

--- sandbox/hello.hs
+++ sandbox/hello.hs
@@ -4,9 +4,10 @@ main = do
   putStrLn "What's your name?"
   putStr "> "
   name <- getLine
-  if name == "quit"
-     then putStrLn "bye"
-     else putStrLn $ "Oh hi there, "  ++ name
-  if name /= "quit"
-     then main
-     else return ()
+  case name of
+       "\\q" ->
+         putStrLn "bye" >> 
+         return ()
+       _ ->
+         putStrLn ("Oh hi there, " ++ name) >> 
+         main
